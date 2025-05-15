c9ffa827c5bbde76054cd12146483700a9ea3b7f
includecctype <zhanthong@gmail.com>
2025-05-15 17:33:01 +0800

    login and registeration working

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+from wtforms import Form, StringField, PasswordField, EmailField
+from wtforms.validators import InputRequired, Length
 
 load_dotenv()
 
@@ -15,6 +17,7 @@ db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tmp', 'datab
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "hellothere"
 
 CORS(app)
 
@@ -22,7 +25,7 @@ db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'Login'
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
@@ -32,9 +35,70 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(db.String(89), unique=False, nullable=False)
 
+class AuthForm(Form):
+    username = StringField([InputRequired(), Length(min=4, max=20)])
+    email = EmailField([InputRequired()])
+    password = PasswordField([InputRequired()])
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+@app.route('/', methods=["GET"])
+def Home():
+    return jsonify({
+        
+    })
+
+@app.route('/login', methods=["GET", "POST"])
+def Login():
+    form = AuthForm()
+    user_response = request.get_json()
+    form.username.data = user_response['username']
+    form.email.data = user_response['email']
+    form.password.data = user_response['password']
+
+    if request.method == "POST" and form.validate():
+        try: # use try except to avoid server crashing
+            user = User.query.filter_by(username = form.username.data).first()
+
+            check_password = bcrypt.check_password_hash(user.password, form.password.data)
+
+            if user and check_password:
+                login_user(user)
+                return jsonify({"message": "Login Successful."})
+            else: 
+                raise Exception()
+        except:
+            return jsonify({"message": "Login Failed. Your credentials does not match any user."})
+        
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    form = AuthForm()
+    user_response = request.get_json()
+    form.username.data = user_response['username']
+    form.email.data = user_response['email']
+    form.password.data = user_response['password']
+
+    if request.method == "POST":
+        try:
+            form.validate()
+
+            hashed_password = bcrypt.generate_password_hash(form.password.data)
+
+            new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+
+            return jsonify({"message": "Registeration Successful"})
+        except:
+            return jsonify({"message": "Registeration Unsuccessful. Your email or username might be duplicated."})
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True, port=5321)
 
 
--- frontend/src/logout/register.jsx
+++ frontend/src/logout/register.jsx
@@ -1,9 +1,54 @@
-
+import { useRef, useEffect, useState } from "react"
 
 export default function Register(){
 
+    const [response, setResponse] = useState(null)
+
+    const form = useRef(null)
+
+    const username = useRef(null)
+    const email = useRef(null)
+    const password = useRef(null) 
+    const submit = useRef(null)
+
+    useEffect(()=>{
+        if(form.current){
+            form.current.addEventListener('submit', async e=>{
+                e.preventDefault()
+
+                let response = await fetch(`${import.meta.env.VITE_BACKEND_URI}/register`, { // if React created with Vite create, use import.meta and use VITE prefix
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        username: username.current.value.trim(),
+                        email: email.current.value.trim(),
+                        password: password.current.value.trim()
+                    })
+                })
+
+                let message = await response.json()
+                if (message.message) setResponse(message.message)
+            })
+        }
+    }, [])
+
     return <>
-        <h1>Register</h1>
+        <form method="POST" ref={form}>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" ref={username}/>
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" ref={email}/>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" ref={password}/>
+            <button type="submit" ref={submit}>Submit</button>
+        </form>
+
+        {(()=>{
+            if (response) return <><p>{response}</p></>
+        })()}
     </>
 
 }
+
