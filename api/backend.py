9e68e6592d276e772f2fe40af4b0d4647257b60e
includecctype <zhanthong@gmail.com>
2025-05-23 01:35:08 +0800

    trying to solve vercel error

--- api/backend.py
+++ api/backend.py
@@ -30,6 +30,7 @@ app.config["SECRET_KEY"] = "hellothere"
 # app.config["SESSION_COOKIE_SECURE"] = False # False for local development
 app.config["SESSION_PERMANENT"] = False
 app.config['SESSION_TYPE'] = "filesystem"
+app.config['SESSION_FILE_DIR'] = '/tmp/flask_session'
 
 CORS(
     app=app,
